bc33bee938e6824d8b172f14ea5b771b50a984ce
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-25 12:00:53 +0500

    vue methods

---
+++ lesson 1/basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+    };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+  },
+});
+
+app.mount("#events");
