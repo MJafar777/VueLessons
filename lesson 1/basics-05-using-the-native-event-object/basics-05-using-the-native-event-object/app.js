11bb0cac3190ae1e58e3ca437b525c107926bdf2
jafar <mirzaraximovjafar2001@gmail.com>
2022-07-25 13:07:43 +0500

    vue propertys

--- lesson 1/basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
+++ lesson 1/basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -10,11 +10,27 @@ const app = Vue.createApp({
       this.name = event.target.value;
     },
     add(num) {
+      console.log("methods");
       this.counter = this.counter + num;
     },
     reduce(num) {
+      console.log("methods");
       this.counter = this.counter - num;
       // this.counter--;
+    },
+  },
+  computed: {
+    onCount() {
+      return this.counter;
+    },
+    onChange() {
+      console.log("computed");
+      return this.name;
+    },
+  },
+  watch: {
+    name(newData, oldData) {
+      return newData + " Mirzaraximov";
     },
   },
 });
