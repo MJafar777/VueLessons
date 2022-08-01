3f076f9dddd3ef9bff7d073b2bc59df7ef7f7f6f
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-01 19:02:25 +0500

    qwer

--- lesson 7 bolim/homework/homework 3/src/App.vue
+++ lesson 7 bolim/homework/homework 3/src/App.vue
@@ -53,6 +53,194 @@ onMounted(() => {
 });
 </script>
 
-<template>app</template>
+<template>
+  <main>
+    <h1>My Anime Tracker</h1>
 
-<style scoped></style>
+    <form @submit.prevent="searchAnime">
+      <input
+        type="text"
+        placeholder="Search for an anime..."
+        v-model="query"
+        @input="handleInput"
+      />
+      <button type="submit" class="button">Search</button>
+    </form>
+
+    <div class="results" v-if="search_results.length > 0">
+      <div v-for="anime in search_results" :key="anime" class="result">
+        <img :src="anime.images.jpg.image_url" />
+        <div class="details">
+          <h3>{{ anime.title }}</h3>
+          <p :title="anime.synopsis" v-if="anime.synopsis">
+            {{ anime.synopsis.slice(0, 120) }}...
+          </p>
+          <span class="flex-1"></span>
+          <button class="button" @click="addAnime(anime)">
+            Add to My Anime
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="myanime" v-if="my_anime.length > 0">
+      <h2>My Anime</h2>
+
+      <div v-for="anime in my_anime_asc" :key="anime" class="anime">
+        <img :src="anime.image" />
+        <h3>{{ anime.title }}</h3>
+        <div class="flex-1"></div>
+        <span class="episodes"
+          >{{ anime.watched_episodes }} / {{ anime.total_episodes }}</span
+        >
+        <button
+          v-if="anime.total_episodes !== anime.watched_episodes"
+          @click="increaseWatch(anime)"
+          class="button"
+        >
+          +
+        </button>
+        <button
+          v-if="anime.watched_episodes > 0"
+          @click="decreaseWatch(anime)"
+          class="button"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+}
+body {
+  background-color: #eee;
+}
+main {
+  margin: 0 auto;
+  max-width: 768px;
+  padding: 1.5rem;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+form {
+  display: flex;
+  max-width: 480px;
+  margin: 0 auto 1.5rem;
+}
+form input {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: white;
+  display: block;
+  color: #888;
+  font-size: 1.125rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+}
+.button {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+  display: block;
+  padding: 0.5rem 1rem;
+  background-image: linear-gradient(to right, deeppink 50%, darkviolet 50%);
+  background-size: 200%;
+  color: white;
+  font-size: 1.125rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  transition: 0.4s;
+}
+.button:hover {
+  background-position: right;
+}
+.results {
+  background-color: #fff;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-height: 480px;
+  overflow-y: scroll;
+  margin-bottom: 1.5rem;
+}
+.result {
+  display: flex;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  transition: 0.4s;
+}
+.result img {
+  width: 100px;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+.details {
+  flex: 1 1 0%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.details h3 {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+}
+.details p {
+  font-size: 0.875rem;
+  margin-bottom: 1rem;
+}
+.details .button {
+  margin-left: auto;
+}
+.flex-1 {
+  display: block;
+  flex: 1 1 0%;
+}
+.myanime h2 {
+  color: #888;
+  font-weight: 400;
+  margin-bottom: 1.5rem;
+}
+.myanime .anime {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  background-color: #fff;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+.anime img {
+  width: 72px;
+  height: 72px;
+  object-fit: cover;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+.anime h3 {
+  color: #888;
+  font-size: 1.125rem;
+}
+.anime .episodes {
+  margin-right: 1rem;
+  color: #888;
+}
+.anime .button:first-of-type {
+  margin-right: 0.5rem;
+}
+.anime .button:last-of-type {
+  margin-right: 0;
+}
+</style>
