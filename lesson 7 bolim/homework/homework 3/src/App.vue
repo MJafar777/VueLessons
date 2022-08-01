ba3cc5d55a86c11556f087063654d4ceb5c5a3f1
jafar <mirzaraximovjafar2001@gmail.com>
2022-08-01 15:52:33 +0500

    qwerty

--- lesson 7 bolim/homework/homework 3/src/App.vue
+++ lesson 7 bolim/homework/homework 3/src/App.vue
@@ -1,4 +1,57 @@
-<script setup></script>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const query = ref("");
+const my_anime = ref([]);
+const search_results = ref([]);
+
+const my_anime_asc = computed(() => {
+  return my_anime.value.sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+});
+
+const searchAnime = () => {
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      search_results.value = res.data;
+    });
+};
+
+const handleInput = (e) => {
+  if (!e.target.value) {
+    search_results.value = [];
+  }
+};
+
+const addAnime = (anime) => {
+  search_results.value = [];
+  query.value = "";
+  my_anime.value.push({
+    id: anime.mal_id,
+    title: anime.title,
+    image: anime.images.jpg.image_url,
+    total_episodes: anime.episodes,
+    watched_episodes: 0,
+  });
+  localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+};
+
+const increaseWatch = (anime) => {
+  anime.watched_episodes++;
+  localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+};
+const decreaseWatch = (anime) => {
+  anime.watched_episodes--;
+  localStorage.setItem("my_anime", JSON.stringify(my_anime.value));
+};
+onMounted(() => {
+  my_anime.value = JSON.parse(localStorage.getItem("my_anime")) || [];
+});
+</script>
 
 <template>app</template>
 
